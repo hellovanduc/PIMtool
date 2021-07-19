eb713d0b165cf574df81ec036d4de856d0260f11
hellovanduc <nguyenduc21022k@gmail.com>
2021-07-19 13:53:44 +0700

    Add slogan to groupDetail

---
+++ Repositories/Migrations/202107190648379_PIMdb_addSlogan.cs
@@ -0,0 +1,20 @@
+﻿namespace Repositories.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PIMdb_addSlogan : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.GroupDetail", "Slogan", c => c.String());
+            DropColumn("dbo.GroupDetail", "MaximumNumberOfEmployee");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.GroupDetail", "MaximumNumberOfEmployee", c => c.Int(nullable: false));
+            DropColumn("dbo.GroupDetail", "Slogan");
+        }
+    }
+}
