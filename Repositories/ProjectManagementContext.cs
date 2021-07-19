3b7e8c06dd1b04e0e4b991ff6254fc83aff3ef74
hellovanduc <nguyenduc21022k@gmail.com>
2021-07-19 07:54:35 +0700

    Change name of db

--- Repositories/ProjectManagementContext.cs
+++ Repositories/ProjectManagementContext.cs
@@ -15,10 +15,10 @@ namespace Repositories
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Group> Groups { get; set; }
         public DbSet<Project> Projects { get; set; }
-
-        // Prevents table names from being pluralized (ex: Employees => Employee)
+         
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            // Prevents table names from being pluralized (ex: Employees => Employee)
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
             modelBuilder.Entity<Group>()
